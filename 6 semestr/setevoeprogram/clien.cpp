a2e2945ef823134f384e7ec35237c6010fbab24b
alan <alan@Ubuntu.myguest.virtualbox.org>
2024-02-04 20:43:42 +0300

    lab1

---
+++ 6 semestr/setevoeprogram/clien.cpp
@@ -0,0 +1,60 @@
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h> 
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define BUFLEN 81
+
+int main(int argc, char *argv[]){
+int sock;
+char message[BUFLEN];
+struct sockaddr_in servAddr, clientAddr;
+struct hostent *hp, *gethostbyname(const char *name);
+
+    if(argc < 4)
+        { 
+            printf( " ВВЕСТИ udpclient имя_хоста порт сообщение\n");
+          exit(1) ;
+        }
+    if( (sock = socket(AF_INET, SOCK_DGRAM, 0 ) ) < 0)
+        { 
+            perror("He могу получить socket\n");
+          exit(1);
+        }
+    bzero((char *) &servAddr, sizeof(servAddr));
+
+    servAddr.sin_family = AF_INET;
+    hp = gethostbyname(argv[1]);
+    bcopy(hp -> h_addr, &servAddr.sin_addr, hp -> h_length);
+    servAddr.sin_port = htons( atoi(argv[2]));
+    bzero( (char *)&clientAddr, sizeof(clientAddr));
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    clientAddr.sin_port = 0 ;
+    if (bind( sock, (struct sockaddr *) &clientAddr, sizeof(clientAddr)))
+    { 
+            perror("Клиент не получил порт.");
+          exit(1);
+    }
+    printf("CLIENT: Готов к пересылке.\n");
+ 	for (int i = 1; i <= 10; ++i) {
+ 	snprintf(message, BUFLEN, "%d", i);
+	    if( sendto ( sock, message, strlen(message), 0 ,(struct sockaddr *) &servAddr , 				 
+	    sizeof(servAddr ) ) < 0 )
+		{   
+		    perror( "Проблемы с sendto . \n" );
+		  exit(1);
+		}
+		printf("Sent: %d\n", i);
+		sleep(1);
+      }
+    printf("CLIENT: Пересылка завершена. Счастливо оставаться. \n");
+    close(sock);
+    }
+    
+
---
+++ 6 semestr/setevoeprogram/server.cpp
@@ -0,0 +1,69 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define BUFLEN 81
+
+int main() {
+    int sockMain, msgLength;
+    socklen_t length;
+    struct sockaddr_in servAddr, clientAddr;
+    char buf[BUFLEN];
+
+    if ((sockMain = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Сервер не может открыть socket для UDP.");
+        exit(1);
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = 0;
+
+    if (bind(sockMain, (struct sockaddr *)&servAddr, sizeof(servAddr))) {
+        perror("Связывание сервера неудачно.");
+        exit(1);
+    }
+
+    length = sizeof(servAddr);
+    if (getsockname(sockMain, (struct sockaddr *)&servAddr, &length)) {
+        perror("Вызов getsockname неудачен.");
+        exit(1);
+    }
+
+    printf("СЕРВЕР: номер порта - %d\n", ntohs(servAddr.sin_port));
+
+    for (;;) {
+        length = sizeof(clientAddr);
+        bzero(buf, sizeof(BUFLEN));
+
+        if ((msgLength = recvfrom(sockMain, buf, BUFLEN, 0, (struct sockaddr *)&clientAddr, &length)) < 0) {
+            perror("Плохой socket клиента.");
+            exit(1);
+        }
+
+        printf("SERVER: IP адрес клиента: %s\n", inet_ntoa(clientAddr.sin_addr));
+        printf("SERVER: PORT клиента: %d\n", ntohs(clientAddr.sin_port));
+        printf("SERVER: Длина сообщения - %d\n", msgLength);
+        printf("SERVER: Сообщение: %s\n", buf);
+
+        // Отправляем обратное подтверждение "выполнено" клиенту
+        const char *response = "выполнено";
+        sendto(sockMain, response, strlen(response), 0, (struct sockaddr *)&clientAddr, length);
+
+        printf("SERVER: Отправлено клиенту: %s\n\n", response);
+    }
+
+    // Закрытие сокета (Note: This part is unreachable in the current implementation)
+    close(sockMain);
+
+    return 0;
+}
+
